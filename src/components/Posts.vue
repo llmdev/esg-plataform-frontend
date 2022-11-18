2bf5e1e02607b3635557a4a3cc4695f9802ce7e5
Paulo Demutti <paulodemutti@gmail.com>
2022-11-18 19:07:53 -0300

    with credentials false

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -11,7 +11,9 @@ export default {
     },
     methods: {
       fetchData() {
-        axios.get('https://api.hubapi.com/content/api/v2/blog-posts?hapikey=demo')
+        axios.get('https://api.hubapi.com/content/api/v2/blog-posts?hapikey=demo',{
+          withCredentials: false,
+        })
           .then(response => {
             this.list = response.data;
             this.fetching = false
@@ -24,18 +26,20 @@ export default {
 }
 </script>
 <template>
-  <h1>
-    TODOS OS POSTS
-  </h1>
-  <span v-if="fetching">Carregando...</span>
+  <section>
 
-  <ul class="list">
-    <li v-for="(item, index) in list.objects" :key="index" class="list-group-item">
-      <span>{{item.label}}</span>
-      <a :href="item.absolute_url">{{item.absolute_url}}</a>
-    </li>
-  </ul>
-  <!-- {{list}} -->
+    <h1>
+      TODOS OS POSTS
+    </h1>
+    <span v-if="fetching">Carregando...</span>
+  
+    <ul class="list">
+      <li v-for="(item, index) in list.objects" :key="index" class="list-group-item">
+        <span>{{item.label}}</span>
+        <a :href="item.absolute_url">{{item.absolute_url}}</a>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <style></style>
